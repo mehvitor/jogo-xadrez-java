a19f2c48911fbbb44835733494324eaab052819e
Vitor Telles Escame <mehvitor93@gmail.com>
2024-06-21 14:31:57 -0300

    Implementado movimentos possíveis para a torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch  {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("Nao existe peca na posicao de origem");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Não existe movimento possiveis para a peca escolhida");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
